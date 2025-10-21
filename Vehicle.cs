9981f2ee417531b05b7b934043e13df40046d5ea
Albert <Albert@LAPTOP-S4EA0KB6>
2025-10-21 12:02:16 +0200

    Add project files.

---
+++ Vehicle.cs
@@ -0,0 +1,31 @@
+﻿namespace Labb2_Arv
+{
+    internal class Vehicle
+    {
+        string _brand = "Unknown";
+        string _model = "Unknown";
+        int _year;
+        double _topSpeed;
+
+        string Brand
+        {
+            get { return _brand; }
+            set { _brand = value; }
+        }
+        string Model
+        {
+            get { return _model; }
+            set { _model = value; }
+        }
+        int Year
+        {
+            get { return _year; }
+            set { _year = value; }
+        }
+        double TopSpeed
+        {
+            get { return _topSpeed; }
+            set { _topSpeed = value; }
+        }
+    }
+}
